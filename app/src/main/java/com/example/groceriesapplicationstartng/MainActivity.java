1cbb19c66bb6358003396877b674939616c60f0f
sayTG <busarithienkie@gmail.com>
2020-04-19 03:02:59 +0100

    Complete task

--- app/src/main/java/com/example/groceriesapplicationstartng/MainActivity.java
+++ app/src/main/java/com/example/groceriesapplicationstartng/MainActivity.java
@@ -5,7 +5,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.BitSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +25,19 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.grape_wine_icon, R.drawable.cherry_icon, R.drawable.kiwi_icon, R.drawable.starfruit_icon, R.drawable.lime_icon};
     MyAdapter adapter;
 
+    Button button;
+
+    public static ArrayList<my_model> modelArrayList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+
+        button = findViewById(R.id.button);
 
         s1 = getResources().getStringArray(R.array.fruit_name);
         s2 = getResources().getStringArray(R.array.fruit_price);
@@ -32,7 +48,29 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        modelArrayList = getModel();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Next_Activity.class);
+                startActivity(intent);
+            }
+        });
 
 
+
+    }
+
+    private ArrayList<my_model> getModel(){
+        ArrayList<my_model> list = new ArrayList<>();
+
+        for (int i = 0 ; i<13 ; i++){
+            my_model model = new my_model();
+            model.setNumber(0);
+            model.setFruit(s1[i]);
+            list.add(model);
+        }
+        return list;
     }
 }
